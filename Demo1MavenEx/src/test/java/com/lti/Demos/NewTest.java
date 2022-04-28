b80b2d9f4d941c8d498f66042e99a4f2cd47f8bd
MohitDhote <mohitdhote99@gmail.com>
2022-04-28 12:35:01 +0530

    second commit

---
+++ Demo1MavenEx/src/test/java/com/lti/Demos/NewTest.java
@@ -0,0 +1,14 @@
+package com.lti.Demos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NewTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
